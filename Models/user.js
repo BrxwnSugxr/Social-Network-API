50ab2f7091542dea2e3d93a3809e6b856ccaf3b4
mo sahid <jsdev.mas@gmail.com>
2024-07-03 09:50:56 +1000

    laptop issue

--- Models/user.js
+++ Models/user.js
@@ -1,44 +1,54 @@
-const mongoose = require("mongoose");
-const validator = require("validator");
+// Importing the required dependencies from mongoose
+const { Schema, model } = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true,
+// Defining the User schema
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      // use REGEX to validate correct email format
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please use a valid email address",
+      ],
+    },
+    // Array of _id values referencing the Thought model
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    // Array of _id values referencing the User model (self-reference)
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
   },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error("Please enter valid email");
-      }
-      return true;
+  {
+    toJSON: {
+      virtuals: true,
     },
-  },
-  //   ref match with model though
-  thoughts: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Thought",
-    },
-  ],
-  //   ref match with model user
-  friends: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-    },
-  ],
-});
+    id: false,
+  }
+);
 
-UserSchema.virtual("friendCount").get(function () {
+//a virtual property `friendCount` that retrieves the length of the user's friends array field on query.
+userSchema.virtual("friendCount").get(function () {
   return this.friends.length;
 });
 
-const User = mongoose.model("User", UserSchema);
+// Create the User model using the User schema
+const User = model("User", userSchema);
 
 module.exports = User;
--- Routes/api.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const userRouters = require("./user");
-const thoughtRoutes = require("./thought");
-const Router = express.Router();
-
-Router.use("/users", userRouters);
-Router.use("/thoughts", thoughtRoutes);
-
-module.exports = Router;

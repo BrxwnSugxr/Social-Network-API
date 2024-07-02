283a2a4ed471b96d3a132dc0353fff061d63e0df
mo sahid <jsdev.mas@gmail.com>
2024-07-03 09:06:17 +1000

    server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+require("./config/db");
+const ApiRoutes = require("./Routes/api");
+const app = express();
+const PORT = process.env.PORT || 3030;
+
+app.use(express.json());
+app.use("/api", ApiRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Working");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});

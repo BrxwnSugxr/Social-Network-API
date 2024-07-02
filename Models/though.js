c5351149dcfa416be4ac9a56eb4eea26a3b0b722
mo sahid <jsdev.mas@gmail.com>
2024-07-03 09:07:35 +1000

    models

---
+++ Models/though.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+// creating user and thought, not creating reaction, adding reaction in scema
+const ReactionSchema = require("./reaction");
+
+const getFormattedDate = (createdDate) => {
+  return new Date(createdDate).toLocaleDateString("en-US");
+};
+
+const ThoughtSchema = new mongoose.Schema({
+  ThoughtText: {
+    type: String,
+    required: true,
+    validate(value) {
+      if (value < 1 || value.length > 280) {
+        throw new Error("please enter thought Text between 1 to 280.");
+      }
+    },
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: getFormattedDate,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  // not inserting ID, inserting the whole object
+  reactions: [ReactionSchema],
+});
+
+ThoughtSchema.methods.toJSON = function () {
+  const thought = this;
+  const newThought = thought.toObject();
+  newThought.createdAt = new Date(newThought.createdAt).toLocaleDateString(
+    "en-US"
+  );
+  return newThought;
+};
+
+ThoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = mongoose.model("Thought", ThoughtSchema);
+
+module.exports = Thought;
---
+++ Models/user.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Please enter valid email");
+      }
+      return true;
+    },
+  },
+  //   ref match with model though
+  thoughts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Thought",
+    },
+  ],
+  //   ref match with model user
+  friends: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+});
+
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
